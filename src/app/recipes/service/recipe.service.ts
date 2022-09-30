54f7b56078462229d2bc05a6f1cfdae7e28fea98
GodStarLord <62302642+GodStarLord@users.noreply.github.com>
2022-09-30 21:46:01 +0530

    Adding Ingredients to Recipes
    Add Ingredients Array to the Recipe Model

--- src/app/recipes/service/recipe.service.ts
+++ src/app/recipes/service/recipe.service.ts
@@ -1,18 +1,21 @@
 import { EventEmitter } from '@angular/core';
 
 import { Recipe } from '../model/recipe.model';
+import { Ingredient } from 'src/app/shared/model/ingredient.model';
 
 export class RecipeService {
   private recipes: Recipe[] = [
     new Recipe(
       'Kunafa Sweet',
       'Kunafa Sweet Description',
-      'https://th.bing.com/th/id/OIP.q93W5wlftaIGoYva7NkdYwHaFi?pid=ImgDet&rs=1'
+      'https://th.bing.com/th/id/OIP.q93W5wlftaIGoYva7NkdYwHaFi?pid=ImgDet&rs=1',
+      [new Ingredient('Bread', 3), new Ingredient('Jam', 1)]
     ),
     new Recipe(
       'Egg Bonda',
       'Egg Bonda Description',
-      'https://3.bp.blogspot.com/-UMhQU4LP5x4/UhCM5aQb59I/AAAAAAAAAS8/EVOaF6P0FZE/s1600/100_3227.JPG'
+      'https://3.bp.blogspot.com/-UMhQU4LP5x4/UhCM5aQb59I/AAAAAAAAAS8/EVOaF6P0FZE/s1600/100_3227.JPG',
+      [new Ingredient('Peanuts', 10), new Ingredient('Berries', 50)]
     ),
   ];
   recipeSelected = new EventEmitter<Recipe>();
