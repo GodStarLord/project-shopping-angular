7237aba5b1abf10b5c2ba330da364a693ed32b80
GodStarLord <62302642+GodStarLord@users.noreply.github.com>
2022-09-30 16:42:27 +0530

    Allowing the User to Add Ingredients to the Shopping List
    - Use Template Form approach

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  EventEmitter,
+  OnInit,
+  Output,
+  ViewChild,
+} from '@angular/core';
+
+import { Ingredient } from 'src/app/shared/model/ingredient.model';
 
 @Component({
   selector: 'app-shopping-edit',
   templateUrl: './shopping-edit.component.html',
-  styleUrls: ['./shopping-edit.component.css']
+  styleUrls: ['./shopping-edit.component.css'],
 })
 export class ShoppingEditComponent implements OnInit {
+  @ViewChild('nameInput') nameInputRef: ElementRef;
+  @ViewChild('amountInput') amountInputRef: ElementRef;
+  @Output() ingredientAdded = new EventEmitter<Ingredient>();
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  onAdd() {
+    const { value: name } = this.nameInputRef.nativeElement;
+    const { value: amount } = this.amountInputRef.nativeElement;
+    const newIngredient = new Ingredient(name, amount);
+
+    this.ingredientAdded.emit(newIngredient);
   }
-
 }
