83be847a4cdde69e45d29e5b3edf1869a5fd6390
GodStarLord <62302642+GodStarLord@users.noreply.github.com>
2022-10-01 12:26:59 +0530

    Adding Editing Routes
    /** Most Specific to Most Generic */
    /** Static Routes followed by Dynamic Route Parameters */

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,15 +4,19 @@ import { RouterModule, Routes } from '@angular/router';
 import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
 import { RecipesComponent } from './recipes/recipes.component';
 import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
+import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 
 /** Most Specific to Most Generic */
+/** Static Routes followed by Dynamic Route Parameters */
 const appRoutes: Routes = [
   {
     path: 'recipes',
     component: RecipesComponent,
     children: [
+      { path: 'new', component: RecipeEditComponent },
       { path: ':id', component: RecipeDetailComponent },
+      { path: ':id/edit', component: RecipeEditComponent },
       { path: '', component: RecipeStartComponent },
     ],
   },
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,7 @@ import { RecipesComponent } from './recipes/recipes.component';
 import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
 import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
 import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
+import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 
@@ -26,6 +27,7 @@ import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-ed
     RecipesComponent,
     RecipeListComponent,
     RecipeDetailComponent,
+    RecipeEditComponent,
     RecipeItemComponent,
     ShoppingListComponent,
     ShoppingEditComponent,
