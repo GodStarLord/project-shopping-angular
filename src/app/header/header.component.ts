46b65f4567941ff1689a313e9de7ae896a8e09cd
GodStarLord <62302642+GodStarLord@users.noreply.github.com>
2022-10-06 17:28:45 +0530

    Storing Recipes
    - store all the recipes into the firebase db

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,9 +1,15 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component } from '@angular/core';
+
+import { DataStorageService } from '../shared/service/data-storage.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: 'header.component.html',
 })
 export class HeaderComponent {
-  
+  constructor(private dataService: DataStorageService) {}
+
+  onSaveData(): void {
+    this.dataService.storeRecipes();
+  }
 }
--- src/app/shared/service/data-storage.service.ts
+++ src/app/shared/service/data-storage.service.ts
@@ -1,6 +1,21 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+import { RecipeService } from 'src/app/recipes/service/recipe.service';
 
 @Injectable({ providedIn: 'root' })
 export class DataStorageService {
-    constructor() {}
+  constructor(private http: HttpClient, private recipeService: RecipeService) {}
+
+  storeRecipes(): void {
+    const recipes = this.recipeService.getRecipes();
+
+    this.http
+      .put(
+        'https://ng-course-recipebook-6c2f4-default-rtdb.europe-west1.firebasedatabase.app/recipes.json',
+        recipes
+      )
+      .subscribe((response) => console.log(response));
+  }
 }
