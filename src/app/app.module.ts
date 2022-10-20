43eb1979a0c37238bb83646afeaee952937f9e6c
GodStarLord <62302642+GodStarLord@users.noreply.github.com>
2022-10-20 15:48:04 +0530

    Setting Up the NgRx Store

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { StoreModule } from '@ngrx/store';
 
 import { AppRoutingModule } from './app-routing.module';
 
@@ -9,6 +10,7 @@ import { HeaderComponent } from './header/header.component';
 
 import { SharedModule } from './shared/shared.module';
 import { CoreModule } from './core.module';
+import { ShoppingListReducer } from './shopping-list/store/shopping-list.reducer';
 
 @NgModule({
   declarations: [AppComponent, HeaderComponent],
@@ -16,7 +18,7 @@ import { CoreModule } from './core.module';
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-
+    StoreModule.forRoot({ shoppingList: ShoppingListReducer }),
     SharedModule,
     CoreModule,
   ],
--- src/app/shopping-list/store/shopping-list.reducer.ts
+++ src/app/shopping-list/store/shopping-list.reducer.ts
@@ -1,18 +1,19 @@
-import { Action } from '@ngrx/store';
-
 import { Ingredient } from 'src/app/shared/model/ingredient.model';
-import { ADD_INGREDIENT } from './shopping-list.actions';
+import * as ShoppingListActions from './shopping-list.actions';
 
 const initialState = {
   ingredients: [new Ingredient('Apples', 5), new Ingredient('Tomato', 2)],
 };
 
-export function ShoppingListReducer(state = initialState, action: Action) {
+export function ShoppingListReducer(
+  state = initialState,
+  action: ShoppingListActions.AddIngredient
+) {
   switch (action.type) {
-    case ADD_INGREDIENT:
+    case ShoppingListActions.ADD_INGREDIENT:
       return {
         ...state,
-        ingredients: [...state.ingredients],
+        ingredients: [...state.ingredients, action.payload],
       };
   }
 }
